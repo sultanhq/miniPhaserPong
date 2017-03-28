a9c0543010ddd89d0e2e60670a85c15cf8b89f53
Rob Adams <robert.andrew.adams@gmail.com>
2017-03-28 12:09:19 +0100

    Added Background divider

--- js/game.js
+++ js/game.js
@@ -1,6 +1,10 @@
 var gameProperties = {
   screenWidth: 32,
   screenHeight: 32,
+
+  dashSize: 2,
+  paddleLeft_x: 1,
+  paddleRight_x: 31,
 };
 
 var graphicsAssets = {
@@ -11,7 +15,13 @@ var graphicsAssets = {
   paddlename: 'paddle',
 };
 
-var mainState = function(game) {};
+var mainState = function(game) {
+  this.backgroundGraphics;
+  this.ballSprite;
+  this.paddleLeftSprite;
+  this.paddleRightSprite;
+};
+
 mainState.prototype = {
   preload: function() {
     game.load.image(graphicsAssets.ballName, graphicsAssets.ballURL);
@@ -19,11 +29,21 @@ mainState.prototype = {
   },
 
   create: function() {
-
+    this.initGraphics();
   },
 
   update: function() {
 
+  },
+
+  initGraphics: function() {
+    this.backgroundGraphics = game.add.graphics(0, 0);
+    this.backgroundGraphics.lineStyle(1, 0xFFFFFF, 1);
+
+    for (var y = 0; y < gameProperties.screenHeight; y += gameProperties.dashSize * 2) {
+      this.backgroundGraphics.moveTo(game.world.centerX, y);
+      this.backgroundGraphics.lineTo(game.world.centerX, y + gameProperties.dashSize);
+    }
   },
 };
 
