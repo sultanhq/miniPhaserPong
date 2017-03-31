5332f5686ae7d38e1a988a77e8fac12e972c4725
Rob Adams <robert.andrew.adams@gmail.com>
2017-03-31 15:49:07 -0300

    Init node and start socket io setup

---
+++ server.js
@@ -0,0 +1,20 @@
+//
+
+var express = require('express')
+  , app = express(app)
+  , server = require('http').createServer(app)
+  , io = require('socket.io')(server);
+
+
+
+// serve static files from the current directory
+app.use(express.static(__dirname));
+
+
+server.listen(8000, function(){
+  console.log('listening on *:8000');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
